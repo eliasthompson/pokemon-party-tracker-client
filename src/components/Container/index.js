5a4c205914f0aa1a81d5ccdd78c07d0e33bcc533
Elias Thompson <email@eliasthompson.com>
2021-03-19 03:05:15 -0700

    Badge support

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,49 +3,86 @@ import sheetrock from 'sheetrock';
 import _ from 'lodash';
 
 import Pokemon from '../Pokemon';
+import bugBadgeIcon from '../../images/bug-badge.png';
+import cliffBadgeIcon from '../../images/cliff-badge.png';
+import fairyBadgeIcon from '../../images/fairy-badge.png';
+import icebergBadgeIcon from '../../images/iceberg-badge.png';
+import plantBadgeIcon from '../../images/plant-badge.png';
+import psychicBadgeIcon from '../../images/psychic-badge.png';
+import rumbleBadgeIcon from '../../images/rumble-badge.png';
 import useGlobalStyles from '../../useGlobalStyles';
 import useStyles from './useStyles';
+import voltageBadgeIcon from '../../images/voltage-badge.png';
 
 export default function Container() {
   const globalStyles = useGlobalStyles();
   const { className, styles } = useStyles();
+  const [badges, setBadges] = useState([[]]);
   const [party, setParty] = useState([[]]);
 
-  const getParty = useCallback(() => {
+  const getSheetData = useCallback(() => {
     sheetrock({
       url: 'https://docs.google.com/spreadsheets/d/1DCzg9CF0unmAl9Bl1zOoEbVqEo8nHPlbP7XV6MJGG20/edit#gid=1224681356',
       query: 'SELECT A, B, C, D, E, F WHERE A IS NOT NULL AND F IS NOT NULL AND C = FALSE ORDER BY A ASC',
       reset: true,
-      callback: (error, options, response) => {
-        if (!error) {
-          const rows = _.get(response, 'raw.table.rows', [[]]);
+      callback: (partyError, partyOptions, partyResponse) => {
+        if (!partyError) {
+          const rows = _.get(partyResponse, 'raw.table.rows', [[]]);
           const mappedRows = _.map(rows, (row) =>_.map(row.c, (cell) => (cell) ? cell.v : cell));
           const length = mappedRows.length;
 
           if (length < 6) {
-
             mappedRows.length = 6;
             mappedRows.fill([], length, 6);
           }
 
           setParty(mappedRows);
         } else {
-          console.log(error);
+          console.log(partyError);
         }
 
-        setTimeout(getParty, 10000);
+        sheetrock({
+          url: 'https://docs.google.com/spreadsheets/d/1DCzg9CF0unmAl9Bl1zOoEbVqEo8nHPlbP7XV6MJGG20/edit#gid=631468007',
+          query: 'SELECT C WHERE A = TRUE',
+          reset: true,
+          callback: (badgesError, badgesOptions, badgesResponse) => {
+            if (!badgesError) {
+              const rows = _.get(badgesResponse, 'raw.table.rows', [[]]);
+              const mappedRows = _.map(rows, (row) =>_.map(row.c, (cell) => cell.v));
+
+              setBadges(mappedRows);
+            } else {
+              console.log(badgesError);
+            }
+          }
+        });
       }
     });
-  }, [setParty]);
+
+    setTimeout(getSheetData, 10000);
+  }, [setBadges, setParty]);
 
   useEffect(() => {
-    getParty();
-  }, [getParty]);
+    getSheetData();
+  }, [getSheetData]);
+
+  console.log(badges)
 
   return (
     <Fragment>
       <main className={ className }>
         { _.map(party, (row, i) => <Pokemon key={ i } className={ className } pokemon={ row } />) }
+
+        <div className={ `${className} badges` }>
+          <img className={ className } src={ bugBadgeIcon } alt="" data-obtained={ badges[0] } />
+          <img className={ className } src={ cliffBadgeIcon } alt="" data-obtained={ badges[2] } />
+          <img className={ className } src={ rumbleBadgeIcon } alt="" data-obtained={ badges[3] } />
+          <img className={ className } src={ plantBadgeIcon } alt="" data-obtained={ badges[4] } />
+          <img className={ className } src={ voltageBadgeIcon } alt="" data-obtained={ badges[1] } />
+          <img className={ className } src={ fairyBadgeIcon } alt="" data-obtained={ badges[5] } />
+          <img className={ className } src={ psychicBadgeIcon } alt="" data-obtained={ badges[6] } />
+          <img className={ className } src={ icebergBadgeIcon } alt="" data-obtained={ badges[7] } />
+        </div>
       </main>
 
       { globalStyles }
--- src/components/Container/useStyles.js
+++ src/components/Container/useStyles.js
@@ -9,9 +9,29 @@ const useStyles = () => {
       justify-content: space-evenly;
       flex-wrap: wrap;
       overflow: hidden;
-      /* background-color: black; */
+      background-color: black;
       width: 466px;
-      height: 128px;
+      height: 176px;
+
+      > div.badges {
+        flex: 1 1 100%;
+        display: flex;
+        justify-content: space-between;
+
+        > img {
+          object-fit: contain;
+          max-width: 48px;
+          max-height: 48px;
+          filter: grayscale(1);
+          opacity: 0.5;
+          transition : filter 0.2s, opacity 0.2s;
+
+          &[data-obtained="true"] {
+            filter: grayscale(0);
+            opacity: 1;
+          }
+        }
+      }
     }
   `;
 };
