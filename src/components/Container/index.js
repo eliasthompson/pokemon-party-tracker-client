bb3066afc07999c7f6b1f34054693a470535e9cc
Elias Thompson <email@eliasthompson.com>
2021-03-21 03:08:04 -0700

    Handles skipped pokemon encounters properly

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -23,7 +23,7 @@ export default function Container() {
   const getSheetData = useCallback(() => {
     sheetrock({
       url: 'https://docs.google.com/spreadsheets/d/1DCzg9CF0unmAl9Bl1zOoEbVqEo8nHPlbP7XV6MJGG20/edit#gid=1224681356',
-      query: 'SELECT A, B, C, D, E, F WHERE A IS NOT NULL AND F IS NOT NULL ORDER BY A ASC',
+      query: 'SELECT A, B, C, D, E, F WHERE A IS NOT NULL AND E != "--" AND F IS NOT NULL ORDER BY A ASC',
       reset: true,
       callback: (partyError, partyOptions, partyResponse) => {
         if (!partyError) {
